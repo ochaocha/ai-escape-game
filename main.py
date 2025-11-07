191c8d444e69f1753cfc2dbbbe0074d5d64a2512
あなたの名前 <you@example.com>
2025-11-07 16:00:43 +0900

    Day3: MongoDB接続 & ユーザー登録API追加

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
+from pydantic import BaseModel
+from passlib.hash import bcrypt 
 
 app = FastAPI()
 
@@ -8,6 +10,34 @@ client = MongoClient(MONGO_URL)
 
 db = client["escape_of_echo_db"]
 players = db["players"]
+
+class PlayerCreate(BaseModel):
+    username: str
+    password: str
+
+@app.post("/player/register")
+def register_player(player: PlayerCreate):
+   hashed_password = bcrypt.hash(player.password)
+   players.insert_one({
+       "username": player.username,
+       "password": hashed_password
+   })
+   return {"message": "登録完了！"," username": player.username}
+
+
+
+class RegisterUser(BaseModel):
+    username: str
+    password: str 
+
+@app.post("/player/register")
+def register_user(user:RegisterUser):
+    if players.find_one({"username": user.username}):
+        return {"error": "Username already exists"}
+    hashed_pw = bcrypt.hash(user.password)
+    players.insert_one({"username": user.username, "password": hashed_pw})
+    return {"message": "User registered successfully"}
+
 
 @app.get("/testdb")
 def test_db():
